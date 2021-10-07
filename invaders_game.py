c83ff769d3184f0f395132be44f1465f61d3b50f
qbarefoot <quintenbarefoot@gmail.com>
2021-10-07 14:55:59 -0500

    set up invaders_game.py main file, installed Pygame

---
+++ invaders_game.py
@@ -0,0 +1,40 @@
+import sys 
+
+import os
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+import pygame
+
+#the class being used to manage the entire game itself
+class invaders_game:
+
+#method used to turn on the game and manage assests
+    def __init__(self):
+        pygame.init()
+
+        #used "pygame.display.set_mode" to make a display window, use the tuple "(1600, 900) for screen dimension", then assigned everything to "self.screen" attribute
+        self.screen = pygame.display.set_mode((800, 800))
+        pygame.display.set_caption("Invaders From Space!")
+
+        #used to mod the background color
+        self.bg_color = (230, 230, 230)
+
+#the main loop for the game
+    def run_game(self):
+        while True:
+            #event listener used to listen for controls to close the game
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #recreate screen during each pass on the loop
+            self.screen.fill(self.bg_color)
+
+            #used to update and make the newest screen visible
+            pygame.display.flip()
+
+#used to run Invaders From Space
+if __name__ == "__main__":
+    ai = invaders_game
+    ai.run_game
