7ee8d1107ba44aadeede1b74252a64806ba974d9
qbarefoot <quintenbarefoot@gmail.com>
2021-10-13 15:51:41 -0500

    created alien fleet, further tweaking to image and settings for placement in future

--- invaders_game.py
+++ invaders_game.py
@@ -34,7 +34,30 @@ class Invaders_Game:
     #used to spawn aliens
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (5* alien_width)
+        number_aliens_x = available_space_x // (5* alien_width)
+
+
+        # used to determine the number of aliens on that can be fit on screen.
+        spaceship_height = self.spaceship.rect.height
+        available_space_y = (self.settings.screen_height -
+                                (5 * alien_height) - spaceship_height)
+        number_rows = available_space_y //  (5* alien_height)
+        
+        # creates the entire alien force
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+    
+    #creates row of aliens 
+    def _create_alien(self, alien_number, row_number):
+            alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
+            alien.x = alien_width + 5* alien_width * alien_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + 5* alien.rect.height * row_number
+            self.aliens.add(alien)
 
 
     #the main loop for the game
