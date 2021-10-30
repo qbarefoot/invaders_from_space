eec9016c0617593abffd9cb0f5d222fd4af3af6b
qbarefoot <quintenbarefoot@gmail.com>
2021-10-30 00:47:02 -0500

    fixed bug where play button is clicked even after it disappears resets game

--- invaders_game.py
+++ invaders_game.py
@@ -66,7 +66,8 @@ class Invaders_Game:
 
     #click "Play" button to start new game and reset stats
     def _check_play_button(self, mouse_pos):
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             self.stats.reset_stats()
             self.stats.game_active = True
 
