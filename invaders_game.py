516726f3ec685bf5c019dc52cc2d267a087fe9a8
qbarefoot <quintenbarefoot@gmail.com>
2022-04-08 14:54:39 -0500

    changed caption in main file to title of game and removed redundant code from settings

--- invaders_game.py
+++ invaders_game.py
@@ -12,7 +12,6 @@ from button import Button
 from pygame.locals import *
 from pygame import mixer
 
-
 #the class being used to manage the entire game itself
 class Invaders_Game:
 
@@ -28,7 +27,7 @@ class Invaders_Game:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Invaders Are Coming!!!")
+        pygame.display.set_caption("Invaders From Space!!!")
 
         #used to store game stats instance
         self.stats = InvaderStats(self)
